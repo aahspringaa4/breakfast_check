eed4449d8701a8f9e6a961f0059d929fc5d01483
yoohyunmyeong <aahspringaa4@naver.com>
2023-11-30 17:31:58 +0900

    feat: add breakfast check screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '조식 체크 앱',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BreakfastCheckPage(),
+    );
+  }
+}
+
+class BreakfastCheckPage extends StatefulWidget {
+  @override
+  _BreakfastCheckPageState createState() => _BreakfastCheckPageState();
+}
+
+class _BreakfastCheckPageState extends State<BreakfastCheckPage> {
+  final List<String> users = ['유현명', '김민채', 'adbr'];
+  Map<String, bool> userChecks = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _loadChecks();
+  }
+
+  _loadChecks() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    users.forEach((user) {
+      userChecks[user] = (prefs.getBool(user) ?? false);
+    });
+    setState(() {});
+  }
+
+  _saveCheck(String user, bool value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool(user, value);
+  }
+
+  void _toggleCheck(String user, bool? value) {
+    if (value != null) {
+      setState(() {
+        userChecks[user] = value;
+        _saveCheck(user, value);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('조식 체크 앱'),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          return CheckboxListTile(
+            title: Text(users[index]),
+            value: userChecks[users[index]],
+            onChanged: (value) => _toggleCheck(users[index], value),
+          );
+        },
       ),
     );
   }
